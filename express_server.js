5bb49ad173c93c8f4b189d2a6d8d753146375262
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2021-01-19 01:40:12 +0000

    body-parser library installed and added a POST request to answer with OK

--- express_server.js
+++ express_server.js
@@ -1,41 +1,55 @@
+function generateRandomString() {
+  return Math.random().toString(16).substr(2, 6);
+}
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs");
-
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase}
+  const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-app.get('/urls/new', function (req, res) {
+app.get("/urls/new", function (req, res) {
   res.render("urls_new");
-})
+});
 
-app.get('/urls/:shortURL', function (req, res) {
-  const templateVars = {shortURL: req.params.shortURL, longURL: req.params.longURL};
+app.get("/urls/:shortURL", function (req, res) {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  };
   res.render("urls_show", templateVars);
-})
-
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
