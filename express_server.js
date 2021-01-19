cf9bdcd15a278ccd56a01ec436a479eec1ea7258
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2021-01-19 20:06:44 +0000

    post method added to delete a specific url created and redirect to url page

--- express_server.js
+++ express_server.js
@@ -5,11 +5,12 @@ function generateRandomString() {
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -37,6 +38,13 @@ app.get("/urls/new", function (req, res) {
   res.render("urls_new");
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL
+  res.redirect(`/urls/${shortURL}`);
+});
+
 app.get("/urls/:shortURL", function (req, res) {
   const templateVars = {
     shortURL: req.params.shortURL,
@@ -45,20 +53,17 @@ app.get("/urls/:shortURL", function (req, res) {
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  let shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body.longURL
-  res.redirect(`/urls/${shortURL}`);
-  
-});
-
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  //console.log(urlDatabase[req.params.shortURL])
+  delete urlDatabase[req.params.shortURL]
+  res.redirect("/urls");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
-  console.log(urlDatabase)
 });
